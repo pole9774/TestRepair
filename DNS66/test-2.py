fff397b30922186dccbd4c805aa1414b8a393c32
Alessandro Poletti <pole97@live.it>
2025-08-05 15:06:29 +0200

    DNS66 test-1, test-2

---
+++ DNS66/test-2.py
@@ -0,0 +1,55 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+# After installing, press and hold to start and then shut down
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\DNS66\\DNS66 v0.4.1.apk",
+    #"app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\DNS66\\DNS66 v0.6.8.apk",
+    "appWaitActivity": "org.jak_linux.dns66.*",
+    "noReset": True,
+    "automationName": "UiAutomator2"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://localhost:4723", options=options)
+driver.implicitly_wait(20)
+
+# test case 2: Start main interface
+
+time.sleep(10)
+
+el = driver.find_elements(AppiumBy.ID, 'org.jak_linux.dns66:id/start_button')[0]
+driver.execute_script("mobile: longClickGesture", {
+    "elementId": el.id,
+    "duration": 5000
+})
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/button1')[0]
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'org.jak_linux.dns66:id/start_button')[0]
+driver.execute_script("mobile: longClickGesture", {
+    "elementId": el.id,
+    "duration": 5000
+})
+time.sleep(1)
+
+el = driver.find_element(AppiumBy.ID, 'org.jak_linux.dns66:id/switch_onboot')
+el.click()
+time.sleep(1)
+
+el = driver.find_element(AppiumBy.ID, 'org.jak_linux.dns66:id/switch_onboot')
+el.click()
+time.sleep(1)
+
+driver.quit()
