95fcaf58cd472a2c5e7718b43c9b0308b57a1f11
Alessandro Poletti <pole97@live.it>
2025-08-06 17:10:18 +0200

    openBikeSharing tests

---
+++ openBikeSharing/test-1.py
@@ -0,0 +1,37 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+# todo: Select a network in advance when entering, and you need to wait a little longer
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\openBikeSharing\\openBikeSharing v1.0.apk",
+    #"app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\openBikeSharing\\openBikeSharing v1.10.0.apk",
+    "appWaitActivity": "be.brunoparmentier.openbikesharing.app.*",
+    "noReset": True,
+    "automationName": "UiAutomator2"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://localhost:4723", options=options)
+driver.implicitly_wait(20)
+
+# test case 1: Main interface
+
+time.sleep(5)
+
+el = driver.find_element(AppiumBy.ID, 'be.brunoparmentier.openbikesharing.app:id/action_refresh')
+el.click()
+time.sleep(1)
+
+el = driver.find_element(AppiumBy.ID, 'be.brunoparmentier.openbikesharing.app:id/action_map')
+el.click()
+time.sleep(1)
+
+driver.quit()
---
+++ openBikeSharing/test-5.py
@@ -0,0 +1,45 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+# todo: Select a network in advance when entering, and you need to wait a little longer
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\openBikeSharing\\openBikeSharing v1.0.apk",
+    #"app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\openBikeSharing\\openBikeSharing v1.10.0.apk",
+    "appWaitActivity": "be.brunoparmentier.openbikesharing.app.*",
+    "noReset": True,
+    "automationName": "UiAutomator2"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://localhost:4723", options=options)
+driver.implicitly_wait(20)
+
+# test case 5: More options---About
+
+time.sleep(15)
+
+el = driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'More options')
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/title')[0]
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/title')[3]
+el.click()
+time.sleep(1)
+
+el = driver.find_element(AppiumBy.ID, 'android:id/button1')
+el.click()
+time.sleep(1)
+
+driver.quit()
