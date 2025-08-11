ef7898439d5adcf75e3ca095c1ac093dd1692d8d
Alessandro Poletti <pole97@live.it>
2025-08-11 16:06:27 +0200

    Counter done

---
+++ Counter/test-1.py
@@ -0,0 +1,33 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "appPackage": "me.tsukanov.counter",
+    "appActivity": "me.tsukanov.counter.ui.MainActivity",
+    "noReset": True,
+    "automationName": "UiAutomator2"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://localhost:4723", options=options)
+
+time.sleep(5)
+
+try:
+    el1 = driver.find_element(AppiumBy.ID, 'android:id/up')
+    el1.click()
+    time.sleep(3)
+
+    el2 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Add counter")')
+    el2.click()
+
+finally:
+    time.sleep(5)
+    driver.quit()
