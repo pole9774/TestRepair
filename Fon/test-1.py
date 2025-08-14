4306f75a70f8cfcd74e847b71b2ebc68f52b0cee
Alessandro Poletti <pole97@live.it>
2025-08-14 15:45:10 +0200

    Fon done

---
+++ Fon/test-1.py
@@ -0,0 +1,33 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "appPackage": "com.oakley.fon",
+    "appActivity": "com.oakley.fon.AndroidWISPr",
+    "noReset": True,
+    "automationName": "UiAutomator2"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://localhost:4723", options=options)
+
+time.sleep(5)
+
+try:
+    # el1 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Username")')
+    # el1.click()
+
+    # branch
+    el1 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Your Fon username")')
+    el1.click()
+
+finally:
+    time.sleep(5)
+    driver.quit()
