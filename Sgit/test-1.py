b85d8e3bb4b14b46c4659173c8e484daadbcb3a3
Alessandro Poletti <pole97@live.it>
2025-08-17 14:08:29 +0200

    Sgit added

---
+++ Sgit/test-1.py
@@ -0,0 +1,34 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "appPackage": "me.sheimi.sgit",
+    "appActivity": "me.sheimi.sgit.RepoListActivity",
+    "noReset": True,
+    "automationName": "UiAutomator2"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://localhost:4723", options=options)
+
+time.sleep(5)
+
+try:
+    el1 = driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'More options')
+    el1.click()
+    time.sleep(3)
+
+    el2 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Private Keys")')
+    el2.click()
+    time.sleep(3)
+
+finally:
+    time.sleep(5)
+    driver.quit()
