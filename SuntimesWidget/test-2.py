47b3c4877db74fb3f418bdf37a66fb68275a816e
Alessandro Poletti <pole97@live.it>
2025-08-19 16:34:24 +0200

    SuntimesWidget tests added

---
+++ SuntimesWidget/test-2.py
@@ -0,0 +1,49 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "appPackage": "com.forrestguice.suntimeswidget",
+    "appActivity": "com.forrestguice.suntimeswidget.SuntimesActivity",
+    "noReset": True,
+    "automationName": "UiAutomator2"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://localhost:4723", options=options)
+
+time.sleep(5)
+
+try:
+    el1 = driver.find_elements(AppiumBy.ID, 'com.forrestguice.suntimeswidget:id/action_location_add')[0]
+    el1.click()
+    time.sleep(3)
+
+    el2 = driver.find_elements(AppiumBy.ID, 'com.forrestguice.suntimeswidget:id/appwidget_location_mode')[0]
+    el2.click()
+    time.sleep(3)
+
+    el3 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Current (last known)")')
+    el3.click()
+    time.sleep(3)
+
+    el4 = driver.find_elements(AppiumBy.ID, 'com.forrestguice.suntimeswidget:id/appwidget_location_mode')[0]
+    el4.click()
+    time.sleep(3)
+
+    el5 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("User Defined")')
+    el5.click()
+    time.sleep(3)
+
+    el6 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Cancel")')
+    el6.click()
+
+finally:
+    time.sleep(3)
+    driver.quit()
