9627706d2863fe3d8bb12a6a3aa65fe3f97952e1
Alessandro Poletti <pole97@live.it>
2025-09-08 18:46:19 +0200

    SimpleCalculator repaired

---
+++ SimpleCalculator/test-3-repaired.py
@@ -0,0 +1,47 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "appPackage": "com.simplemobiletools.calculator",
+    "appActivity": "com.simplemobiletools.calculator.activities.MainActivity",
+    "noReset": True,
+    "automationName": "UiAutomator2"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://localhost:4723", options=options)
+
+time.sleep(5)
+
+try:
+    el1 = driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'More options')
+    el1.click()
+    time.sleep(3)
+
+    el2 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Settings")')
+    el2.click()
+    time.sleep(3)
+
+    el3 = driver.find_elements(AppiumBy.ID, 'com.simplemobiletools.calculator:id/settings_customize_colors_label')[0]
+    el3.click()
+    time.sleep(3)
+
+    # Updated theme name from "Light theme" to "Light"
+    el4 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Light")')
+    el4.click()
+    time.sleep(3)
+
+    # Updated theme name from "Dark theme" to "Dark"
+    el5 = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Dark")')
+    el5.click()
+
+finally:
+    time.sleep(5)
+    driver.quit()
