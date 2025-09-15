ab9730914b1b8ee8c958d6f8dbb53fda94eccbbd
Alessandro Poletti <pole97@live.it>
2025-09-15 17:46:10 +0200

    Mgit healenium done

---
+++ Mgit/test-2-new-healenium.py
@@ -0,0 +1,44 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    #"app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\Mgit\\Mgit v1.5.1.apk",
+    "app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\Mgit\\Mgit v1.6.1.apk",
+    "appWaitActivity": "com.manichord.mgit.*",
+    "noReset": True,
+    "automationName": "UiAutomator2",
+    "healenium:session": True,
+	"sessionKey": "Mgit_test-2-new"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://127.0.0.1:8085", options=options)
+driver.implicitly_wait(20)
+
+# test case 1: Search
+
+time.sleep(15)
+
+el = driver.find_element(AppiumBy.ID, 'com.manichord.mgit:id/action_search')
+el.click()
+time.sleep(1)
+
+el = driver.find_element(AppiumBy.ID, 'android:id/search_src_text')
+el.send_keys('test')
+time.sleep(1)
+
+driver.press_keycode(66) # ENTER
+time.sleep(1)
+
+el = driver.find_element(AppiumBy.ID, 'android:id/up')
+el.click()
+time.sleep(1)
+
+driver.quit()
