6073e79dbd9b7f8ac638b616a13fb23ed5e8c3a0
Alessandro Poletti <pole97@live.it>
2025-09-15 18:13:46 +0200

    openBikeSharing healenium done

---
+++ openBikeSharing/test-3-healenium.py
@@ -0,0 +1,82 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+# todo: Select a network in advance when entering, and you need to wait a little longer
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    #"app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\openBikeSharing\\openBikeSharing v1.0.apk",
+    "app": "C:\\Users\\pole9\\Desktop\\Poli\\Tesi\\TestRepair\\openBikeSharing\\openBikeSharing v1.10.0.apk",
+    "appWaitActivity": "be.brunoparmentier.openbikesharing.app.*",
+    "noReset": True,
+    "automationName": "UiAutomator2",
+    "healenium:session": True,
+	"sessionKey": "openBikeSharing_test-3"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://127.0.0.1:8085", options=options)
+driver.implicitly_wait(20)
+
+# test case 3: More options - Map layer
+
+time.sleep(5)
+
+el = driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'More options')
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/title')[0] # Settings
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/title')[1] # Map layer
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/text1')[0] # Mapnik
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/title')[1] # Map layer
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/text1')[1] # OpenCycleMap
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/title')[1] # Map layer
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/text1')[2] # OSMPublicTransport
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/title')[1] # Map layer
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/text1')[3] # MapQuest-OSM
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/title')[1] # Map layer
+el.click()
+time.sleep(1)
+
+el = driver.find_elements(AppiumBy.ID, 'android:id/text1')[0] # Mapnik
+el.click()
+time.sleep(1)
+
+driver.back()
+time.sleep(1)
+
+driver.quit()
