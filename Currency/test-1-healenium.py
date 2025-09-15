0ca49da976724287e497d91f5dae64cf75370a93
Alessandro Poletti <pole97@live.it>
2025-09-15 14:15:13 +0200

    Currency healenium done

---
+++ Currency/test-1-healenium.py
@@ -0,0 +1,31 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "appPackage": "org.billthefarmer.currency",
+    "appActivity": "org.billthefarmer.currency.Main",
+    "noReset": True,
+    "automationName": "UiAutomator2",
+    "healenium:session": True,
+	"sessionKey": "Currency_test-1"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://127.0.0.1:8085", options=options)
+
+time.sleep(5)
+
+try:
+    el1 = driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Settings')
+    el1.click()
+
+finally:
+    time.sleep(5)
+    driver.quit()
---
+++ Currency/test-2-healenium.py
@@ -0,0 +1,35 @@
+import time
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "emulator-5554",
+    "appPackage": "org.billthefarmer.currency",
+    "appActivity": "org.billthefarmer.currency.Main",
+    "noReset": True,
+    "automationName": "UiAutomator2",
+    "healenium:session": True,
+	"sessionKey": "Currency_test-2"
+}
+
+options = UiAutomator2Options()
+for key, value in desired_caps.items():
+    options.set_capability(key, value)
+
+driver = webdriver.Remote("http://127.0.0.1:8085", options=options)
+
+time.sleep(5)
+
+try:
+    el1 = driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Settings')
+    el1.click()
+    time.sleep(3)
+
+    el2 = driver.find_elements(AppiumBy.CLASS_NAME, 'android.widget.TextView')[10] # About
+    el2.click()
+
+finally:
+    time.sleep(5)
+    driver.quit()
